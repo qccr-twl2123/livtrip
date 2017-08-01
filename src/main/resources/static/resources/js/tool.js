882d22464decf69215d8a07d73cd3d3c81795232
mark1xie <54355534453@qq.com>
2017-08-01 16:22:02 +0800

    add formToObject

--- src/main/resources/static/resources/js/tool.js
+++ src/main/resources/static/resources/js/tool.js
@@ -75,7 +75,24 @@ $(function(){
             }
         });
     };
-
-
-
 });
+/**
+ * 表单参数化
+ * @param formId
+ * @returns {{}}
+ */
+function formToObject(formId){
+    var o={};
+    var a=$("#"+formId).serializeArray();
+    $.each(a,function(){
+        if(o[this.name]){
+            if(!o[this.name].push){
+                o[this.name]=[o[this.name]];
+            }
+            o[this.name].push(this.value||'');
+        }else{
+            o[this.name]=this.value||'';
+        }
+    });
+    return o;
+}
