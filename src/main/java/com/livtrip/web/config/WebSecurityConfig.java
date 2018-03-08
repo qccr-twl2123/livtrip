c99b3a0b605760ce42af85cb838ecc1a39db4fac
mark1xie <545739504@qq.com>
2018-03-08 11:45:22 +0800

    fixbug csrf

--- src/main/java/com/livtrip/web/config/WebSecurityConfig.java
+++ src/main/java/com/livtrip/web/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http    .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","/index.html","/order.html","/resources/**","/login/signIn").permitAll()
                 .anyRequest().authenticated()
